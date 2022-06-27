50be6adef5b6edea54c21c597a066e30c9ed1450
MoonHKLee <mf311110@gmail.com>
2022-06-27 23:31:18 +0900

    (fix) : 메인함수 clear.

--- src/main/java/numberBaseball/Main.java
+++ src/main/java/numberBaseball/Main.java
@@ -9,8 +9,6 @@ import java.io.OutputStreamWriter;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write("숫자를 입력해주세요 : ");
+
     }
 }
